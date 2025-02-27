4af657e25952037de44a1360a09947c3d7278e2d
hyerinleee <hyerin.lee@im-fine.co.kr>
2025-02-27 17:49:01 +0900

    init

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "IMFINE-TerraXPacMan_ARGame-0.1.12";
+const cacheName = "IMFINE-TerraXPacMan_ARGame-0.1.13";
 const contentToCache = [
     "Build/final2.loader.js",
     "Build/final2.framework.js.unityweb",
